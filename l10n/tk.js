8a1c3715fd0c4a2d8596cca1f92131b7b8640ff7
loveniamonteiro <139855484+loveniamonteiro@users.noreply.github.com>
2023-07-19 14:33:17 +0900

    Add files via upload

---
+++ l10n/tk.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "analytics",
+    {
+    "Delete" : "Pozmak",
+    "Search" : "Gözlemek",
+    "OK" : "Bolýar",
+    "Cancel" : "ýatyrmak",
+    "Add to favorites" : "Saýlananlara goşuň",
+    "Report" : "Hasabat ugradyň",
+    "Share" : "Paýlaş",
+    "Share link" : "Baglanyşyk paýlaş",
+    "Link copied" : "Link-yň nusgasy alyndy",
+    "Type" : "Görnüşi",
+    "File" : "Faýl",
+    "Yes" : "Hawa",
+    "No" : "Ýok",
+    "Password" : "Açarsöz",
+    "Favorites" : "Halanýanlar",
+    "Translate" : "Terjime et",
+    "Back" : "Yzyna",
+    "Settings" : "Sazlamalar",
+    "Name" : "Ady",
+    "Unshare" : "Paýlaşma",
+    "Copy link" : "Link-y nusgalaň",
+    "Create" : "Dörediň"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/ur_PK.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "analytics",
+    {
+    "Delete" : "حذف کریں",
+    "Search" : "Search",
+    "OK" : "اوکے",
+    "Cancel" : "منسوخ کریں",
+    "Add" : "شامل کریں",
+    "Remove from favorites" : "Remove from favorites",
+    "Add to favorites" : "Add to favorites",
+    "Share" : "تقسیم",
+    "Share link" : "اشتراک لنک",
+    "Type" : "Type",
+    "Date" : "Date",
+    "File" : "File",
+    "Yes" : "Yes",
+    "No" : "No",
+    "Password" : "پاسورڈ",
+    "Favorites" : "Favorites",
+    "Settings" : "سیٹینگز",
+    "Name" : "اسم",
+    "Advanced" : "ایڈوانسڈ",
+    "Title" : "عنوان",
+    "Unshare" : "شئیرنگ ختم کریں",
+    "Password protection" : "Password protection"
+},
+"nplurals=2; plural=(n != 1);");
